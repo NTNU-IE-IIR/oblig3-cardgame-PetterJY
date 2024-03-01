ca77e3cc636eaf17ebb88206468245b6f7cde40e
Petter J. Ytterdahl <petterjy@hotmail.no>
2024-03-01 11:51:16 +0100

    Changed from Changed from fixed array to arraylist. Added basic functionality to the dealHand button in the ui. UI close to alpha.

--- src/main/java/no/ntnu/idatx2003/oblig3/cardgame/DeckOfCards.java
+++ src/main/java/no/ntnu/idatx2003/oblig3/cardgame/DeckOfCards.java
@@ -4,41 +4,43 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.Random;
-import java.util.random.*;
 
 public class DeckOfCards {
   private final char[] suits = { 'S', 'H', 'D', 'C' };
   private final char[] faces = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13 };
-  private final PlayingCard[] deck;
+  private final ArrayList<PlayingCard> deck;
 
   /**
    * Constructor for the DeckOfCards.
    */
   public DeckOfCards() {
-    this.deck = new PlayingCard[52];
-    int i = 0;
+    this.deck = new ArrayList<>(52);
     for (char suit : this.suits) {
       for (char face : this.faces) {
-        this.deck[i++] = new PlayingCard(suit, face);
+        this.deck.add(new PlayingCard(suit, face));
       }
     }
   }
 
   /**
-   * Returns  random cards from the deck.
-   * Number of cards returned is specified by the parameter numberOfRandomCards.
+   * Returns a deck of n random cards.
+   * The cards are removed from the deck.
    *
    * @param numberOfRandomCards the number of random cards to return.
    * @return an array of random cards.
    */
-  public PlayingCard[] dealHand(int numberOfRandomCards) {
-    PlayingCard[] randomDeck = new PlayingCard[numberOfRandomCards]; // Create a new array of random cards.
-    ArrayList<PlayingCard> deckList = new ArrayList<PlayingCard>(Arrays.asList(this.deck));
+  public ArrayList<PlayingCard> dealHand(int numberOfRandomCards) {
+    if (numberOfRandomCards > this.deck.size()) {
+      throw new IllegalArgumentException("Number of cards to deal is greater than the number of cards in the deck.");
+    } else if (numberOfRandomCards <= 0) {
+      throw new IllegalArgumentException("Number of cards to deal cannot be negative or 0.");
+    }
+    ArrayList<PlayingCard> randomDeck = new ArrayList<>(); // Create a new array list for the random deck.
     Random rand = new Random(); // Create a new random object.
     for (int i = 0; i < numberOfRandomCards; i++) { // Loop through the random deck.
-      int randomIndex = rand.nextInt(deckList.size()); // Get a random index from the deck list.
-      randomDeck[i] = deckList.get(randomIndex); // Add the card to the random deck.
-      deckList.remove(randomIndex); // Remove the card from the deck list.
+      int randomIndex = rand.nextInt(this.deck.size()); // Get a random index from the deck list.
+      randomDeck.add(this.deck.get(randomIndex)); // Add the card to the random deck.
+      this.deck.remove(randomIndex); // Remove the card from the deck list.
     }
     return randomDeck;
   }
@@ -49,7 +51,7 @@ public class DeckOfCards {
    * @return the deck of cards.
    */
   public Iterator<PlayingCard> getDeck() {
-    ArrayList<PlayingCard> deckList = new ArrayList<PlayingCard>(Arrays.asList(this.deck));
+    ArrayList<PlayingCard> deckList = new ArrayList<PlayingCard>(this.deck);
     return deckList.iterator();
   }
 }
--- src/main/java/no/ntnu/idatx2003/oblig3/cardgame/DeckOfCardsUI.java
+++ src/main/java/no/ntnu/idatx2003/oblig3/cardgame/DeckOfCardsUI.java
@@ -1,61 +1,140 @@
 package no.ntnu.idatx2003.oblig3.cardgame;
 
 import javafx.application.Application;
-import javafx.scene.Group;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
+import java.util.ArrayList;
+
 public class DeckOfCardsUI extends Application {
+  private DeckOfCards deck; // The deck of cards.
+  public DeckOfCardsUI() {
+    this.deck = new DeckOfCards(); // Create a new deck of cards.
+  }
   @Override
   public void start(Stage stage) throws Exception {
-
     //Buttons
-    Button startButton = new Button("Start Game");
-    Button quitButton = new Button("Quit Game");
+    Button dealHand = new Button("Deal Hand");
+    Button checkHand = new Button("Check Hand");
+
+    //Text fields
+    TextField sumOfTheFaces = new TextField();
+    TextField cardsOfHearts = new TextField();
+    TextField flush = new TextField();
+    TextField queenOfSpades = new TextField();
 
     //BorderPane
-    BorderPane borderMenu = new BorderPane();
+    BorderPane borderPane = new BorderPane();
 
     //Setting boxes
-    HBox topMenu = new HBox();
-    VBox leftMenu = new VBox();
-    VBox rightMenu = new VBox();
-    HBox bottomMenu = new HBox();
+    VBox center = new VBox();
+    VBox right = new VBox();
+    HBox bottom = new HBox();
+
+    //Setting GridPanes
+    GridPane subBottom = new GridPane();
+    GridPane subCenter = new GridPane();
+
+    //Setting the position of the GridPanes
+    subBottom.setPadding(new Insets(10, 10, 10, 10));
+    subBottom.setHgap(10);
+    subBottom.setVgap(10);
+
+    subCenter.setPadding(new Insets(10, 10, 10, 10));
+    subCenter.setHgap(10);
+    subCenter.setVgap(10);
 
     //Setting the size of the boxes
-    topMenu.setPrefSize(300, 50);
-    leftMenu.setPrefSize(100, 300);
-    rightMenu.setPrefSize(100, 300);
-    bottomMenu.setPrefSize(300, 50);
+    center.setPrefSize(300, 200);
+    right.setPrefSize(100, 300);
+    bottom.setPrefSize(300, 50);
 
     //Adding colors to the boxes
-    topMenu.setStyle("-fx-background-color: #336699;");
-    leftMenu.setStyle("-fx-background-color: #190257;");
-    rightMenu.setStyle("-fx-background-color: #682734;");
-    bottomMenu.setStyle("-fx-background-color: #942374;");
+    //Set to White for now
+    center.setStyle("-fx-background-color: #FFFFFF");
+    right.setStyle("-fx-background-color: #FFFFFF");
+    bottom.setStyle("-fx-background-color: #FFFFFF");
 
     //Adding buttons to the boxes
-    leftMenu.getChildren().add(startButton);
-    leftMenu.getChildren().add(quitButton);
+    right.getChildren().add(dealHand);
+    right.getChildren().add(checkHand);
+
+    //Adding labels to the cards
+    Label card1 = new Label();
+    Label card2 = new Label();
+    Label card3 = new Label();
+    Label card4 = new Label();
+    Label card5 = new Label();
+
+    //Adding the card labels to the subCenter GridPane
+    //To be centered in the middle of the center box
+    subCenter.add(card1, 0, 0);
+    subCenter.add(card2, 1, 0);
+    subCenter.add(card3, 2, 0);
+    subCenter.add(card4, 3, 0);
+    subCenter.add(card5, 4, 0);
+
+    //Setting alignment of the subCenter GridPane
+    subCenter.setAlignment(Pos.CENTER);
+
+    //Adding action to dealHand button
+    try {
+    dealHand.setOnAction(e -> {
+      ArrayList<PlayingCard> hand = this.deck.dealHand(5);
+      card1.setText(hand.get(0).getAsString());
+      card2.setText(hand.get(1).getAsString());
+      card3.setText(hand.get(2).getAsString());
+      card4.setText(hand.get(3).getAsString());
+      card5.setText(hand.get(4).getAsString());
+    });} catch (IllegalArgumentException e) {
+      System.out.println(e.getMessage());
+    }
+
+    //Adding GridPanes to the boxes
+    bottom.getChildren().addAll(subBottom);
+    center.getChildren().addAll(subCenter);
+
+    //Setting alignment of the boxes
+    center.setAlignment(Pos.CENTER);
+    bottom.setAlignment(Pos.CENTER);
+    right.setAlignment(Pos.CENTER);
+
+    //Adding text fields to the GridPanes
+    subBottom.add(sumOfTheFaces, 1, 0);
+    subBottom.add(cardsOfHearts, 3, 0);
+    subBottom.add(flush, 1, 1);
+    subBottom.add(queenOfSpades, 3, 1);
+
+    //Adding labels to the text fields
+    Label sumOfTheFacesLabel = new Label("Sum of the faces:");
+    Label cardsOfHeartsLabel = new Label("Cards of hearts:");
+    Label flushLabel = new Label("Flush:");
+    Label queenOfSpadesLabel = new Label("Queen of spades:");
+
+    //Adding labels to the GridPanes
+    subBottom.add(sumOfTheFacesLabel, 0, 0);
+    subBottom.add(cardsOfHeartsLabel, 2, 0);
+    subBottom.add(flushLabel, 0, 1);
+    subBottom.add(queenOfSpadesLabel, 2, 1);
 
     //Adding boxes to the border
-    borderMenu.setTop(topMenu);
-    borderMenu.setLeft(leftMenu);
-    borderMenu.setRight(rightMenu);
-    borderMenu.setBottom(bottomMenu);
+    borderPane.setCenter(center);
+    borderPane.setRight(right);
+    borderPane.setBottom(bottom);
 
-    //Root
-    FlowPane rootNodeMenu = new FlowPane();
-
-    //Setting colors to the groups
-
-    rootNodeMenu.getChildren().addAll(borderMenu);
-
-    Scene scene = new Scene(rootNodeMenu, 300, 200);
+    //Adding minimum size to the stage
+    Scene scene = new Scene(borderPane, 300, 200);
     stage.setScene(scene);
     stage.setTitle("Card Game");
+    stage.setMinHeight(300);
+    stage.setMinWidth(300);
+    stage.show();
     stage.show();
   }
 
